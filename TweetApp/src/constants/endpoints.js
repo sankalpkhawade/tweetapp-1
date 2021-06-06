cd2013e26b53358e76fadd825861ea782cec0f72
abhi16897 <saiabhishek1997@gmail.com>
2021-06-07 02:36:39 +0530

    changes added

--- TweetApp/src/constants/endpoints.js
+++ TweetApp/src/constants/endpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URI ='http://localhost:8090';
+export const BASE_URI ='http://18.222.202.34:8088/tweet';
 export const BASE_TWEET_URL = '/api/v1.0/tweets'
 export const FORGOT_PASSWD = '/forgetPassword'
 export const AUTHENTICATE = '/authenticate'; 
